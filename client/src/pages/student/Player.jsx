00004051d08e336c371bcd7559958557e13e3802
Nadimul Haque <nadimulhaque05@gmail.com>
2025-02-02 01:51:02 +0530

    Front-end Complete 85%

---
+++ client/src/pages/student/Player.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <>
+    <div className='p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36'>
+      {/* Left Column  */}
+      <div className='text-gray-800'>
+        <h2 className='text-xl font-semibold'>Course Structure</h2>
+      </div>
+
+      {/* right column  */}
+
+      <div>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Player
